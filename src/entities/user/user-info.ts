d38c0d7154cdfc040a5fa836abcd7283e2fc0497
jiashunming <jiashunming@outlook.com>
2018-09-03 16:53:28 +0800

    update: change struct

--- src/entities/user/user-info.ts
+++ src/entities/user/user-info.ts
@@ -7,23 +7,23 @@ export default class UserInfo extends Model<UserInfo> {
   @Column({
     comment: '自增ID',
     autoIncrement: true,
-    primaryKey: true
+    primaryKey: true,
   })
   uid: number
 
   @Column({
-    comment: '姓名'
+    comment: '姓名',
   })
   name: string
 
   @Column({
     comment: '年龄',
-    defaultValue: 0
+    defaultValue: 0,
   })
   age: number
 
   @Column({
-    comment: '性别'
+    comment: '性别',
   })
   gender: number
 }
--- src/models/user/user-info.ts
+++ src/models/user/user-info.ts
@@ -1,8 +1,9 @@
 import path from 'path'
 import { MysqlConfig } from 'config'
-import { configs } from '../../config'
 import { Sequelize } from 'sequelize-typescript'
-export { default as UserInfoModel } from '../../entity/user/user-info'
+import { configs } from '../../config'
+
+export { default as UserInfoModel } from '../../entities/user/user-info'
 
 const mysqlConfig = configs.mysql as MysqlConfig
 
@@ -11,15 +12,18 @@ export default new Sequelize({
   database: mysqlConfig.database,
   username: mysqlConfig.user,
   password: mysqlConfig.password,
-  dialect: 'mysql', // 或者一些其他的数据库
-  modelPaths: [path.resolve(__dirname, `../../entity/${mysqlConfig.entity}`)], // 加载我们的实体
+  // 或者一些其他的数据库
+  dialect: 'mysql',
+  // 加载我们的实体
+  modelPaths: [path.resolve(__dirname, `../../entities/${mysqlConfig.entity}`)],
   pool: {
     // 连接池的一些相关配置
     max: 5,
     min: 0,
     acquire: 30000,
-    idle: 10000
+    idle: 10000,
   },
   operatorsAliases: false,
-  logging: true // true会在控制台打印每次sequelize操作时对应的SQL命令
+  // true会在控制台打印每次sequelize操作时对应的SQL命令
+  logging: true,
 })
